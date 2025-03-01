ed6b320d3386eda89b723b24f7e33d1fed827f0f
Tornike Gomareli <24585160+tornikegomareli@users.noreply.github.com>
2025-03-02 00:05:08 +0400

    initial test view

---
+++ Sources/Overlayer/Overlayer.swift
@@ -0,0 +1,14 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+import SwiftUI
+
+struct TestView: View {
+    var body: some View {
+        Text("232")
+    }
+}
+
+#Preview {
+    TestView()
+}
