f4c5788217f9485a8778fd0f0ff91326292db39a
Tornike Gomareli <24585160+tornikegomareli@users.noreply.github.com>
2025-03-02 17:21:16 +0400

    implemented observation instead of published

--- Sources/Overlayer/OverlayerModifier.swift
+++ Sources/Overlayer/OverlayerModifier.swift
@@ -1,19 +1,72 @@
+//
 //  OverlayerModifier.swift
+//  Overlayer
+//
+//  Created by Tornike Gomareli on 02.03.25.
+//
+
 import SwiftUI
-import Observation
 
-public struct OverlayerModifier<ViewContent: View>: ViewModifier {
+struct OverlayerOptionalModifier<Item: Equatable, ViewContent: View>: ViewModifier {
   var animation: Animation
-  @Binding var isPresented: Bool
-  @ViewBuilder var viewContent: ViewContent
+  @Binding var item: Item?
+  @ViewBuilder var viewContent: (Item) -> ViewContent
   
-  @Environment(OverlayerManager.self) public var properties
+  @Environment(OverlayerManager.self) private var properties
+  @State private var viewID: String?
+  @State private var currentItem: Item?
+  
+  func body(content: Content) -> some View {
+    content
+      .onChange(of: item) { oldValue, newValue in
+        if let newValue {
+          addView(for: newValue)
+        } else {
+          removeView()
+        }
+      }
+  }
+  
+  private func addView(for item: Item) {
+    if properties.window != nil {
+      if viewID == nil {
+        viewID = UUID().uuidString
+      }
+      
+      guard let viewID else { return }
+      currentItem = item
+      
+      removeView()
+      
+      withAnimation(animation) {
+        properties.views.append(.init(id: viewID, view: .init(viewContent(item))))
+      }
+    }
+  }
+  
+  private func removeView() {
+    if let viewID {
+      withAnimation(animation) {
+        properties.views.removeAll(where: { $0.id == viewID })
+      }
+      
+      currentItem = nil
+    }
+  }
+}
 
+
+struct OverlayerModifier<ViewContent: View>: ViewModifier {
+  var animation: Animation
+  @Binding var show: Bool
+  @ViewBuilder var viewContent: ViewContent
+
+  @Environment(OverlayerManager.self) private var properties
   @State private var viewID: String?
   
-  public func body(content: Content) -> some View {
+  func body(content: Content) -> some View {
     content
-      .onChange(of: isPresented, initial: true) { oldValue, newValue in
+      .onChange(of: show, initial: true) { oldValue, newValue in
         if newValue {
           addView()
         } else {
