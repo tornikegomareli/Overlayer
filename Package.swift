2a6b186d888b4c9de3ebcc129f25f63995e097a0
Tornike Gomareli <24585160+tornikegomareli@users.noreply.github.com>
2025-03-02 17:21:58 +0400

    minimum platform versions

--- Package.swift
+++ Package.swift
@@ -6,18 +6,18 @@ import PackageDescription
 let package = Package(
     name: "Overlayer",
     platforms: [
-        .iOS(.v14),
-        .macOS(.v11)
+      .iOS(.v17),
+      .watchOS(.v10),
+      .macOS(.v15),
+      .tvOS(.v15),
+      .visionOS(.v1)
     ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "Overlayer",
             targets: ["Overlayer"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "Overlayer"),
         .testTarget(
