1c24e0b948f7f50afcadc0eba2ae7ee579749736
Tornike Gomareli <24585160+tornikegomareli@users.noreply.github.com>
2025-03-02 17:21:02 +0400

    observable manager for saving views and root window

--- Sources/Overlayer/OverlayerManager.swift
+++ Sources/Overlayer/OverlayerManager.swift
@@ -1,13 +1,20 @@
+//
 //  OverlayerManager.swift
+//  Overlayer
+//
+//  Created by Tornike Gomareli on 02.03.25.
+//
+
 import SwiftUI
+import Observation
 
 @Observable
-final class OverlayerManager {
-    var window: UIWindow?
-    fileprivate var views: [OverlayView] = []
-    
-    struct OverlayView: Identifiable {
-        let id: String
-        let view: AnyView
-    }
+public class OverlayerManager {
+  var window: UIWindow?
+  var views: [OverlayView] = []
+  
+  struct OverlayView: Identifiable {
+    var id: String = UUID().uuidString
+    var view: AnyView
+  }
 }
