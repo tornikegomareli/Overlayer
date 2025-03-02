c5842ca557f103c7828d18258068f02c0a7735c9
Tornike Gomareli <24585160+tornikegomareli@users.noreply.github.com>
2025-03-02 17:21:36 +0400

    ios 18 edge case handling

--- Sources/Overlayer/PassThroughWindow.swift
+++ Sources/Overlayer/PassThroughWindow.swift
@@ -5,16 +5,25 @@
 //  Created by Tornike Gomareli on 02.03.25.
 //
 
-
-//  PassThroughWindow.swift
 import UIKit
 
-final class PassThroughWindow: UIWindow {
-    override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
-        guard let hitView = super.hitTest(point, with: event),
-              let rootView = rootViewController?.view
-        else { return nil }
-        
-        return hitView == rootView ? nil : hitView
+class PassThroughWindow: UIWindow {
+  override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
+    guard let hitView = super.hitTest(point, with: event),
+          let rootView = rootViewController?.view
+    else { return nil }
+    
+    if #available(iOS 18, *) {
+      for subview in rootView.subviews.reversed() {
+        let pointInSubView = subview.convert(point, from: rootView)
+        if subview.hitTest(pointInSubView, with: event) != nil {
+          return hitView
+        }
+      }
+      
+      return nil
+    } else {
+      return hitView == rootView ? nil : hitView
     }
+  }
 }
